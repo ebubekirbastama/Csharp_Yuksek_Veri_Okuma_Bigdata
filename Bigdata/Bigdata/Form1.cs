4c2cec91695a95efc04f001d5a281a88ff2f2714
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2023-07-10 11:31:14 +0300

    Add files via upload

---
+++ Bigdata/Bigdata/Form1.cs
@@ -0,0 +1,52 @@
+﻿using BekraRamclear;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Bigdata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        Thread th;
+        OpenFileDialog op;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            op = new OpenFileDialog();
+            if (op.ShowDialog()==DialogResult.OK)
+            {
+                th = new Thread(oku); th.Start();
+            }
+        }
+        StringBuilder s = new StringBuilder();
+        private void oku()
+        {
+            int syc = 1;
+            using (FileStream fs = File.Open(op.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                //BekraRamClearr.temizle();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    GC.Collect();
+                    GC.WaitForFullGCApproach();
+                    s.AppendLine(line + ",");
+                    syc++;
+                    label1.Text = syc.ToString();
+                    GC.Collect();
+                    GC.WaitForFullGCApproach();
+                    //richTextBox1.AppendText(line);
+                }
+                BekraRamClearr.temizle();
+            }
+        }
+    }
+}
---
+++ Bigdata/Bigdata/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Bigdata
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        OpenFileDialog op;Thread th;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            op = new OpenFileDialog();
+            if (op.ShowDialog()==DialogResult.OK)
+            {
+                th = new Thread(bsl); th.Start();
+            }
+        }
+        private void bsl()
+        {
+            using (FileStream fs = File.Open(op.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    GC.Collect();
+                    GC.WaitForFullGCApproach();
+
+                    richTextBox1.AppendText(line+"\r");
+
+                    GC.Collect();
+                    GC.WaitForFullGCApproach();
+                    
+                }
+            }
+        }
+    }
+}
